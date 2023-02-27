d95b6cae250829ad8f8f01b7833b4fac40d8918a
laindomJS <adrijar21@gmail.com>
2023-02-27 11:02:06 -0400

    feat: added states and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 export const App = () => {
+	const [lineOne, setLineOne] = useState('');
+	const [lineTwo, setLineTwo] = useState('');
+  const [string, setString] = useState('fire');
+
+  const onChangeImage = (evt) => {
+    setString(evt.target.value);
+  }
+
 	return (
 		<div className='App'>
-
-			<select name='meme' id='selectMeme'>
+			
+      <select name='meme' id='selectMeme' value={string} onChange={onChangeImage}>
 				<option value='fire'>Fire</option>
 				<option value='futurama'>Futurama</option>
 				<option value='history'>History</option>
@@ -13,17 +22,27 @@ export const App = () => {
 				<option value='smart'>Smart Guy</option>
 			</select>
 
-			<input type='text' placeholder='Write first line...' />
-			<input type='text' placeholder='Write second line...' />
+			<input
+				type='text'
+				value={lineOne}
+				onChange={(evt) => setLineOne(evt.target.value)}
+				placeholder='Write first line...'
+			/>
 
-      <div>
-        <p>First Line</p>
-        <p>Second Line</p>
-        <img src="" alt="" />
-      </div>
+			<input
+				type='text'
+				value={lineTwo}
+				onChange={(evt) => setLineTwo(evt.target.value)}
+				placeholder='Write second line...'
+			/>
 
-      <button>Export</button>
+			<div>
+				<p>{lineOne}</p>
+				<p>{lineTwo}</p>
+				<img src={`/src/assets/images/${string}.jpg` } />
+			</div>
 
+			<button>Export</button>
 		</div>
 	)
 }
